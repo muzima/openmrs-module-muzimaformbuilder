46ed77d601ecb208de217abfbeed6471c4bf7e42
Sara Eisa <prosarahgamal@outlook.com>
2020-02-10 01:04:42 +0200

    MFB-28
    Feat: add attributes component

---
+++ omod/src/main/muzimaFormBuilderNGApp/src/app/attributes/attributes.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-attributes',
+  templateUrl: './attributes.component.html',
+  styleUrls: ['./attributes.component.css']
+})
+export class AttributesComponent implements OnInit {
+
+  field = {
+    visible: true,
+    id: 'patient.idd',
+    name: 'input1',
+    text: '',
+    dataConcept: '',
+    fieldLbl: 'Enter Name',
+    defaultTxt: '',
+    type: 'select',
+    customCssClass: ['class', 'class2'],
+    readonly: false,
+    required: true,
+    conditionalLogic: [],
+    range: '',
+    inputMask: '',
+    errorMsg: '',
+    description: '',
+    options: [
+      {
+        value: 'female',
+        selected: true
+      },
+      {
+        value: 'male',
+        selected: false
+      }
+    ]
+  };
+
+  dataConcepts = [
+      'data',
+      'concept'
+  ];
+
+  fontSizeControl = new FormControl(13);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addOption() {
+    this.field.options.push({
+      value: 'binary',
+      selected: false
+    });
+  }
+
+}
