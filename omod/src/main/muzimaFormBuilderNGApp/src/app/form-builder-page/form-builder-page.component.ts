9b3a27bf3512e07f80cc5da989a1e57187c6d22c
Sara Eisa <prosarahgamal@outlook.com>
2020-02-10 01:10:51 +0200

    MFB-28
    Feat: add form builder page component

---
+++ omod/src/main/muzimaFormBuilderNGApp/src/app/form-builder-page/form-builder-page.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-builder-page',
+  templateUrl: './form-builder-page.component.html',
+  styleUrls: ['./form-builder-page.component.css']
+})
+export class FormBuilderPageComponent implements OnInit {
+
+  categories = [
+    {
+      name: 'General',
+      fields: [
+        {
+          visible: true,
+          id: '',
+          name: 'input 3',
+          text: '',
+          dataConcept: '',
+          fieldLbl: '',
+          defaultTxt: '',
+          type: '',
+          customCssClass: [],
+          readonly: false,
+          required: false,
+          conditionalLogic: [],
+          range: '',
+          inputMask: '',
+          errorMsg: '',
+          description: ''
+        }
+      ]
+    }
+  ];
+
+  form = {
+    fields: [
+      {
+        visible: true,
+        id: '',
+        name: 'input1',
+        text: '',
+        dataConcept: '',
+        fieldLbl: 'Enter Name',
+        defaultTxt: '',
+        type: 'text',
+        customCssClass: [],
+        readonly: false,
+        required: true,
+        conditionalLogic: [],
+        range: '',
+        inputMask: '',
+        errorMsg: '',
+        description: ''
+      },
+      {
+        visible: true,
+        id: '',
+        name: 'input 2',
+        text: '',
+        dataConcept: '',
+        fieldLbl: '',
+        defaultTxt: '',
+        type: '',
+        customCssClass: [],
+        readonly: false,
+        required: false,
+        conditionalLogic: [],
+        range: '',
+        inputMask: '',
+        errorMsg: '',
+        description: ''
+      }
+    ]
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
